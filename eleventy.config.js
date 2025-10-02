b976f7db1abb97962720449235b523836cc58a42
Josh Storey <josh.storey@airelogic.com>
2025-10-02 16:16:35 +0100

    added pathPrefix

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 import { nhsukEleventyPlugin } from '@x-govuk/nhsuk-eleventy-plugin'
 
 export default function(eleventyConfig) {
+    //Register the plugin
   eleventyConfig.addPlugin(nhsukEleventyPlugin, {
     header: {
       service: {
@@ -13,6 +14,7 @@ export default function(eleventyConfig) {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
+    pathPrefix: "/docs/",
     dir: {
       // The folder where all your content will live:
       input: 'docs',
